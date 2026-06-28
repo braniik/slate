41e2cb70487b24344cfba9da4fe32a0ac6d4f77e
braniik <braniik69@proton.me>
2026-06-28 10:58:14 +0200

    subtle small app fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "0.7.3"
+        versionName = "0.7.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
